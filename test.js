0581668eb6a12f97885e2a71fbe846ecef31f000
ekaterinafilinav <34095532+ekaterinafilinav@users.noreply.github.com>
2021-12-22 16:39:43 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+var answer2 = confirm('Ты учишься в кванториуме?')
+var answer3 = confirm('Ты любишь математику?')
+var answer4 = confirm('Учишься в группе у Екатерины Владимировны?')
+if ((answer1 === true) && (answer2 === true) && (answer3 === true) && (answer4 === true)){
+alert('У тебя будет блестящее будущее')
+}
+var x = 10
+var y = '10'
+console.log(x==y)
+console.log(x===y)
